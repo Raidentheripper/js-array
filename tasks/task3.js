1cc0e3594b4589ba1a8002b2b8ebb7d0227bd8d4
OleksiyZaluzhnyi <al3xpr0gram1ng@gmail.com>
2025-09-22 12:02:09 +0300

    Task solution

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,11 @@
 // З масива людей(people) Вам потрібно знайти людину за іменем(name)
 function findByName(people, name) {
-  // Ваш код
+  for (const person of people) {
+    if (person.name === name) {
+      return person;
+    }
+  }
+  return undefined;
 }
 
 module.exports = findByName;
